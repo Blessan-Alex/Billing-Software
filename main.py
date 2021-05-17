18bca57386cd65d5b4cc75790a1c730da4e8f142
Blessan-Alex <bloxgames7@gmail.com>
2021-05-17 20:41:30 +0530

    Made it compatible for 1280x1080, changed icons pathway, minor tweaks to size

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 # GUI FILE
-from ui_main_cv_1 import Ui_MainWindow
+from ui_main import Ui_MainWindow
 
 # IMPORT FUNCTIONS
 from ui_functions import *
